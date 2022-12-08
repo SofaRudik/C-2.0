547ecb4e4b441ca67feb90c8c235f65bce24bf46
sofia <sonrud@mail.ru>
2022-12-08 15:43:17 +0300

    add dopolnitelnoe

---
+++ Example001_Into/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+
+int n;
+int max1=0, max2=0;
+do
+{    
+     n= int.Parse(Console.ReadLine());
+    if(n>max1)
+     { max2=max1; 
+     max1=n; }
+}while (n!=0);
+ Console.WriteLine(max2);
