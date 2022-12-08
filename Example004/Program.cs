fdb44751b72dfa1dc064c58628f26a8d34564448
sofia <sonrud@mail.ru>
2022-12-08 19:05:39 +0300

    add 004

---
+++ Example004/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine());
+
+if (n==6 || n==7)
+Console.Write("Выходной");
+else if (n<=5 && n>0) Console.Write("Будни");
+else Console.Write("Введите цифру,обозначающую день недели");
